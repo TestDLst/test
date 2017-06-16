a2d88b2cd5855fc56d2968c4504b82a12d2d7043
default <default@example.com>
2017-06-16 18:44:44 +0000

    Исправлен метод _mark_by_regexp

--- request_module.py
+++ request_module.py
@@ -1,17 +1,15 @@
 import re
-import json
 
 
 class RequestAnalyzer:
     # TODO: отмечать параметры запросах REST стиля
-    def __init__(self, request, injection_mark='§ §', verbose=True):
+    def __init__(self, request, injection_mark='§ §'):
         """Создает экзепляр класса RequestAnalyzer
 
         :param request: строка, содержащая сырой валидный запрос к серверу (например запросы из burpsuite)
         :param injection_mark: символы, разделенные пробелом, помечающие точки инъекции
         """
         self.injection_mark = injection_mark
-        self.verbose = verbose
 
         self.excluded_headers = {'Host'}  # Если можно будет указывать, какие параметры пропускать
         self.all_headers = set()  # Все имена распарсенных хидеров будут здесь
@@ -60,17 +58,16 @@ class RequestAnalyzer:
                             or (';' in value and '=' not in value):
                         value = self.injection_mark.replace(' ', value)
                     else:
-                        value = self._mark_by_regexp(value, '=([\S]+);?', prefix='=')
+                        value = self._mark_by_regexp(value, '=([^\s;]+);?')
 
             except ValueError as ve:
-                if self.verbose:
-                    print('Exception in _mark_headers. Message: {}'.format(ve))
+                print('[!] Exception in _mark_headers. Message: {}'.format(ve))
 
             modified_headers.append(': '.join([name, value]))
 
         self.request_object.headers = modified_headers
 
-    # TODO: Парсить xml и json
+    # TODO: Парсить xml
     # TODO: И ещё что-нибудь
     def _mark_data(self):
         """Помечает параметры в данных"""
@@ -87,19 +84,19 @@ class RequestAnalyzer:
 
     def _mark_data_plain(self):
         """Помечаются данные вида param1=value1&param2=value2"""
-        self.request_object.data = self._mark_by_regexp(self.request_object.data, '=([^&]+)', '=')
+        self.request_object.data = self._mark_by_regexp(self.request_object.data, '=([^&]+)')
         self.request_object.data = self._mark_empty_params(self.request_object.data)
 
     def _mark_data_json(self):
+        """Помечаются данные, представленные json"""
         _regexp = '''[ \[]"?([^{}\]\[]+?)["',}]'''
-        #self.request_object.data = self._mark_by_regexp(self.request_object.data, _regexp)
-        #print(self.request_object.data)
-        print(re.findall(_regexp, self.request_object.data))
+        self.request_object.data = self._mark_by_regexp(self.request_object.data, _regexp)
 
     def _mark_data_xml(self):
+        """Помечаются данные, представленные xml"""
         pass
 
-    def _mark_by_regexp(self, string, regexp, prefix=''):
+    def _mark_by_regexp(self, string, regexp, prefix='', group=1):
         """Помечает параметры в строке по regexp'у
 
         :param string: Строка, в которой помечаются параметры
@@ -107,7 +104,10 @@ class RequestAnalyzer:
         :param prefix: Префикс строки, на которую заменяется найденная группа
         :return: Измененная строка string
         """
-        string = re.sub(regexp, lambda x: prefix + self.injection_mark.replace(' ', x.group(1)), string)
+        string = re.sub(regexp,
+                        lambda x: prefix + x.group(0).replace(x.group(group),
+                                                              self.injection_mark.replace(' ', x.group(group))),
+                        string)
         return string
 
     def _mark_empty_params(self, string):
@@ -142,10 +142,11 @@ class RequestObject:
         self.headers, self.data = raw_request.split('\n\n')
         self.query_string, self.headers = self.headers.split('\n')[0], self.headers.split('\n')[1:]
 
-        # Находим хидер Content-type и парсим оттуда type и subtype
+
         self._identify_content_type()
 
     def _identify_content_type(self):
+        """Находит хидер Content-type, парсит type и subtype и определяет по known_types форму данных"""
         content_type = next((header for header in self.headers if header.startswith('Content-Type')), None)
 
         if content_type:
