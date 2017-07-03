82a6386e78c0fcf8701f27518a7321224ee3cb6f
default <default@example.com>
2017-07-03 18:26:36 +0000

    Структуризация

--- request_package/request_marker.py
+++ request_package/request_marker.py
@@ -1,9 +1,9 @@
 import json
 import re
 
-from bs4 import BeautifulSoup
+from request_object import RequestObject
 
-from json_mark import MyJSONEncoder
+from request_package.json_mark import MyJSONEncoder
 
 
 class RequestAnalyzer:
@@ -144,50 +144,3 @@ class RequestAnalyzer:
         :return: Измененная строка string
         """
         return re.sub('=(&|$)', lambda x: '=' + self.injection_mark + ('&' if '&' in x.group() else ''), string)
-
-
-class RequestObject:
-    def __init__(self, request):
-        self.raw_request = request
-        self.market_request = ''
-
-        self.query_string = ''
-        self.headers = ''
-        self.content_type = 'plain'
-        self.data = ''
-
-        self.known_types = {'text': {'html': 'plain', 'plain': 'plain', 'xml': 'xml'},
-                            'application': {'atom+xml': 'xml', 'json': 'json', 'soap+xml': 'xml', 'xhtml+xml': 'xml',
-                                            'xml-dtd': 'xml', 'xop+xml': 'xml', 'xml': 'xml'}}
-
-        self._parse_request(self.raw_request)
-
-    def _parse_request(self, raw_request):
-        """ Распаковывает сырой запрос в объект"""
-        # Разбиваем сырой запрос на 'строку запроса', 'хидеры' и 'дату'
-        # на винде лажа с \r\n, остаются \n при считывании
-        try:
-            self.headers, self.data = raw_request.split('\n\n')
-        except ValueError as ve:
-            self.headers, self.data = raw_request, None
-
-        self.query_string, self.headers = self.headers.split('\n')[0], [x for x in self.headers.split('\n')[1:] if x]
-
-        self._identify_content_type()
-
-    def _identify_content_type(self):
-        """Находит хидер Content-type, парсит type и subtype и определяет по known_types форму данных"""
-        content_type = next((header for header in self.headers if header.startswith('Content-Type')), None)
-
-        if content_type:
-            type, subtype = content_type.split(': ')[1].split('/')
-            self.content_type = self.known_types.get(type).get(subtype)
-
-
-if __name__ == '__main__':
-    with open('request_json.txt') as f:
-        request_string = f.read()
-
-    ra = RequestAnalyzer(request_string)
-    print(ra.get_marked_request())
-    exit()
---
+++ request_package/request_object.py
@@ -0,0 +1,35 @@
+class RequestObject:
+    def __init__(self, request):
+        self.raw_request = request
+        self.market_request = ''
+
+        self.query_string = ''
+        self.headers = ''
+        self.content_type = 'plain'
+        self.data = ''
+        self.known_types = {'text': {'html': 'plain', 'plain': 'plain', 'xml': 'xml'},
+                            'application': {'atom+xml': 'xml', 'json': 'json', 'soap+xml': 'xml', 'xhtml+xml': 'xml',
+                                            'xml-dtd': 'xml', 'xop+xml': 'xml', 'xml': 'xml'}}
+
+        self._parse_request(self.raw_request)
+
+    def _parse_request(self, raw_request):
+        """ Распаковывает сырой запрос в объект"""
+        # Разбиваем сырой запрос на 'строку запроса', 'хидеры' и 'дату'
+        # на винде лажа с \r\n, остаются \n при считывании
+        try:
+            self.headers, self.data = raw_request.split('\n\n')
+        except ValueError as ve:
+            self.headers, self.data = raw_request, None
+
+        self.query_string, self.headers = self.headers.split('\n')[0], [x for x in self.headers.split('\n')[1:] if x]
+
+        self._identify_content_type()
+
+    def _identify_content_type(self):
+        """Находит хидер Content-type, парсит type и subtype и определяет по known_types форму данных"""
+        content_type = next((header for header in self.headers if header.startswith('Content-Type')), None)
+
+        if content_type:
+            type, subtype = content_type.split(': ')[1].split('/')
+            self.content_type = self.known_types.get(type).get(subtype)
