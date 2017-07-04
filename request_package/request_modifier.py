31b457ed252d37f0fb18a41521f6399655a38e87
default <default@example.com>
2017-07-04 10:44:02 +0000

    Добавил заглушки для RequestModifier

---
+++ request_package/request_modifier.py
@@ -0,0 +1,10 @@
+class RequestModifier:
+    def __init__(self, request_object, payloads):
+        pass
+
+    def get_modified_requests(self):
+        """ Возвращает список измененных запросов
+
+        :return: Список request_object'ов с измененными параметрами
+        """
+        return []
