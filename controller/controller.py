d09bea1b52862de49517a5796fffa5c2f38ecbac
1 <1>
2017-07-07 15:55:28 +0200

    переместил get_modified_requests и get_payloads из Controller в Analyzer

--- controller/controller.py
+++ controller/controller.py
@@ -6,6 +6,7 @@ from request_package.request_marker import RequestMarker
 from request_package.requester import Requester
 from core.analyzer import Analyzer
 
+
 # TODO: добавить в Requester метку end в конец очереди после завершения запросов
 class Controller:
     def __init__(self, config):
@@ -14,34 +15,27 @@ class Controller:
         self.initial_request = self.get_initial_request()
         self.marked_raw_request = RequestMarker(self.initial_request, self.config).get_marked_request()
 
-        # Логика
-        # Тест RequestModifier'а
-        meta_payloads = self.get_payloads('/fuzzing/test.txt')
-        self.modified_requests = self.get_modified_requests(meta_payloads)
-        # print(self.modified_requests[0]._testing_param)
-        # exit()
-        # Тест Requester
-        self.response_queue = Queue()
-        requester = Requester(self.modified_requests, self.response_queue, self.config)
-        requester.wait_completion()
-        # print("end")
+        analyzer = Analyzer(self.marked_raw_request)
 
-        # Тест Analyzer
-        analyzer = Analyzer(self.response_queue)
-        analyzer.print_info()
-        print("end")
+        # # Логика
+        # # Тест RequestModifier'а
+        # meta_payloads = self.get_payloads('/fuzzing/test.txt')
+        # self.modified_requests = self.get_modified_requests(meta_payloads)
+        # # print(self.modified_requests[0]._testing_param)
+        # # exit()
+        # # Тест Requester
+        # self.response_queue = Queue()
+        # requester = Requester(self.modified_requests, self.response_queue, self.config)
+        # requester.wait_completion()
+        # # print("end")
+        #
+        # # Тест Analyzer
+        # analyzer = Analyzer(self.response_queue)
+        # analyzer.print_info()
+        # print("end")
 
     def search_hidden_parameters(self):
         pass
-
-    def get_modified_requests(self, payloads):
-        """ Возвращает список модифицированных запросов
-
-        :param payloads: Нагрузки, дополняющие помеченные параметры
-        :return: Список объектов RequestObject
-        """
-        request_modifier = RequestModifier(self.marked_raw_request, payloads, self.config)
-        return request_modifier.get_modified_requests()
 
     def get_initial_request(self):
         """ Возвращает инициализирующий запрос
@@ -51,13 +45,3 @@ class Controller:
         with open(self.config['Main']['file']) as f:
             initial_request = f.read()
         return RequestObject(initial_request)
-
-    def get_payloads(self, payload_path):
-        """
-
-        :param payload_path: путь до файла с нагрузками относительно дирестории payloads
-        :return: Список нагрузок payloads
-        """
-        with open(self.config['Program']['script_path']+'/payloads' + payload_path) as f:
-            payloads = f.read().split('\n')
-        return payloads
