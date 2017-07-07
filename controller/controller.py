6e614fdfe3b9608c9aa277e1181501a807a6c3fc
1 <1>
2017-07-07 16:48:02 +0200

    удалил лишнии функции из Controller

--- controller/controller.py
+++ controller/controller.py
@@ -1,10 +1,7 @@
-from queue import Queue
-
-from request_package.request_modifier import RequestModifier
 from request_package.request_object import RequestObject
 from request_package.request_marker import RequestMarker
-from request_package.requester import Requester
 from core.analyzer import Analyzer
+
 
 # TODO: добавить в Requester метку end в конец очереди после завершения запросов
 class Controller:
@@ -30,15 +27,6 @@ class Controller:
     def search_hidden_parameters(self):
         pass
 
-    def get_modified_requests(self, payloads):
-        """ Возвращает список модифицированных запросов
-
-        :param payloads: Нагрузки, дополняющие помеченные параметры
-        :return: Список объектов RequestObject
-        """
-        request_modifier = RequestModifier(self.marked_raw_request, payloads, self.config)
-        return request_modifier.get_modified_requests()
-
     def get_initial_request(self):
         """ Возвращает инициализирующий запрос
 
@@ -47,13 +35,3 @@ class Controller:
         with open(self.config['Main']['file']) as f:
             initial_request = f.read()
         return RequestObject(initial_request)
-
-    def get_payloads(self, payload_path):
-        """
-
-        :param payload_path: путь до файла с нагрузками относительно дирестории payloads
-        :return: Список нагрузок payloads
-        """
-        with open(self.config['Program']['script_path']+'/payloads' + payload_path) as f:
-            payloads = f.read().split('\n')
-        return payloads
