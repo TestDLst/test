fc131689a11d0f0a82fff532520219795f3d6365
default <default@example.com>
2017-07-08 14:34:25 +0000

    Структуризация анализера 1

--- core/analyzer.py
+++ core/analyzer.py
@@ -1,3 +1,8 @@
+import random
+import re
+import string
+
+
 from queue import Queue
 
 from request_package.request_object import RequestObject
@@ -46,5 +51,47 @@ class Analyzer:
             initial_request = f.read()
         return RequestObject(initial_request)
 
+    def get_standard_response(self):
+        requester = Requester([self.get_initial_request()], self.response_queue, self.config)
+        standard_response = requester.get_standard_response(self.init_request_obj)
+        return standard_response
+
     def analyze(self):
         raise Exception('Не реализовано')
+
+    def clean_reflected_rows(self, response_obj):
+        raw_response = response_obj.raw_response
+        for reflect_pattern in self.reflected_rows:
+            try:
+                raw_response = re.sub(reflect_pattern, '', raw_response)
+                response_obj.rebuild(raw_response)
+            except Exception as e:
+                print(reflect_pattern)
+                print(e)
+        return response_obj
+
+    def detect_reflected_params(self):
+        resp_queue = Queue()
+
+        self._reflect_payload = ''.join([random.choice(string.ascii_letters) for i in range(6)])
+        requests = self.get_modified_requests([self._reflect_payload])
+
+        requester = Requester(requests,resp_queue,self.config)
+        requester.run()
+
+        while requester.is_running() or not resp_queue.empty():
+            resp = resp_queue.get()
+            pattern = '\s+?.+?({reflected}).+?\n'.format(reflected=self._reflect_payload)
+            re.sub(pattern, self._feed_reflected_rows, resp.raw_response)
+
+    def _feed_reflected_rows(self, match):
+        start, _ = match.regs[0]
+        stop, _ = match.regs[1]
+        reflect_pattern = match.string[start:stop] + '.+?\n'
+        reflect_pattern = reflect_pattern.replace('"', '\\"') # экранируем двойные кавычки
+        reflect_pattern = reflect_pattern.replace('(', '\\(').replace(')','\\)') # экранируем скобки
+        reflect_pattern = reflect_pattern.replace('[', '\\[').replace(']','\\]') # экранируем скобки
+        reflect_pattern = reflect_pattern.replace('$', '\\$')
+        reflect_pattern = reflect_pattern.replace('^', '\\^')
+
+        self.reflected_rows |= set([reflect_pattern])
--- core/sql_analyzer.py
+++ core/sql_analyzer.py
@@ -25,48 +25,8 @@ class SqlAnalyzer(Analyzer):
             response_obj = self.clean_reflected_rows(response_obj)
             print(response_obj.testing_param, response_obj.request_time, response_obj.content_length)
 
-    def get_standard_response(self):
-        requester = Requester([self.get_initial_request()], self.response_queue, self.config)
-        standard_response = requester.get_standard_response(self.init_request_obj)
-        return standard_response
+
 
     # requester.wait_completion()
 
     # TODO: удалять строки из ответов, которые изменили внутри свое значение
-    def detect_reflected_params(self):
-        resp_queue = Queue()
-
-        self._reflect_payload = ''.join([random.choice(string.ascii_letters) for i in range(6)])
-        requests = self.get_modified_requests([self._reflect_payload])
-
-        requester = Requester(requests,resp_queue,self.config)
-        requester.run()
-
-        while requester.is_running() or not resp_queue.empty():
-            resp = resp_queue.get()
-            pattern = '\s+?.+?({reflected}).+?\n'.format(reflected=self._reflect_payload)
-            re.sub(pattern, self._feed_reflected_rows, resp.raw_response)
-
-
-    def clean_reflected_rows(self, response_obj):
-        raw_response = response_obj.raw_response
-        for reflect_pattern in self.reflected_rows:
-            try:
-                raw_response = re.sub(reflect_pattern, '', raw_response)
-                response_obj.rebuild(raw_response)
-            except Exception as e:
-                print(reflect_pattern)
-                print(e)
-        return response_obj
-
-    def _feed_reflected_rows(self, match):
-        start, _ = match.regs[0]
-        stop, _ = match.regs[1]
-        reflect_pattern = match.string[start:stop] + '.+?\n'
-        reflect_pattern = reflect_pattern.replace('"', '\\"') # экранируем двойные кавычки
-        reflect_pattern = reflect_pattern.replace('(', '\\(').replace(')','\\)') # экранируем скобки
-        reflect_pattern = reflect_pattern.replace('[', '\\[').replace(']','\\]') # экранируем скобки
-        reflect_pattern = reflect_pattern.replace('$', '\\$')
-        reflect_pattern = reflect_pattern.replace('^', '\\^')
-
-        self.reflected_rows |= set([reflect_pattern])
